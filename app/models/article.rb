a0e8dd8ae3a1bd1363cdd2d7259de660b26830dc
shusakukamiyama <xyz000shupi000@gmail.com>
2019-05-04 16:24:20 +0900

    add association user and articles and add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
-class Article < ActiveRecord::Base
+class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true, length: {in:3..50} 
     validates :description, presence: true, length: {in:10..300} 
+    validates :user_id, presence: true
 end
