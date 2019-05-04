03baede97b50ff71bcc3125d98e2c0271397e380
shusakukamiyama <xyz000shupi000@gmail.com>
2019-05-04 17:52:20 +0900

    add sign-up features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/' => 'pages#home'
   get 'about' => 'pages#about'
+  get 'signup' => 'users#new'
   resources :articles
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
